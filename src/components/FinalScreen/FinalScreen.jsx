92708a70c4a7747ffd854840d5cfc60aedd78940
Lucas Lacerda <lucaslacerda@192.168.1.10>
2024-06-10 13:22:17 -0300

    Outros ajustes na FinalScreen

--- src/components/FinalScreen/FinalScreen.jsx
+++ src/components/FinalScreen/FinalScreen.jsx
@@ -14,23 +14,25 @@ const FinalScreen = ({ candidates, error, onRestart }) => {
   return (
     <div className="FinalScreen">
       <h2>Candidatos Encontrados:</h2>
-      <ul>
+      <div className="candidate-list">
         {candidates.map((candidate) => (
-          <li key={candidate.id} className="candidate-card">
-            <p><strong>ID:</strong> {candidate.id}</p>
-            <p><strong>Cidade ID:</strong> {candidate.cidade_id}</p>
-            <p><strong>Gênero:</strong> {candidate.genero}</p>
-            <p><strong>Experiência Relevante:</strong> {candidate.experienciaRelevante ? 'Sim' : 'Não'}</p>
-            <p><strong>Matriculado na Faculdade:</strong> {candidate.matriculadoFaculdade}</p>
-            <p><strong>Escolaridade:</strong> {candidate.escolaridade}</p>
-            <p><strong>Tempo de Experiência:</strong> {candidate.tempoDeExperiencia} meses</p>
-            <p><strong>Tempo no Último Emprego:</strong> {candidate.tempoNoUltimoEmprego} meses</p>
-            <p><strong>Horas de Treinamento:</strong> {candidate.horasDeTreinamento}</p>
-            <p><strong>Último Salário:</strong> R${candidate.ultimoSalario}</p>
-            <p><strong>Percentual de Compatibilidade:</strong> {candidate.percentualCompatibilidade}%</p>
-          </li>
+          <div key={candidate.id} className="candidate-card">
+            <div className="candidate-detail">
+              <p><strong>ID:</strong> {candidate.id}</p>
+              <p><strong>Cidade ID:</strong> {candidate.cidade_id}</p>
+              <p><strong>Gênero:</strong> {candidate.genero}</p>
+              <p><strong>Experiência Relevante:</strong> {candidate.experienciaRelevante ? 'Sim' : 'Não'}</p>
+              <p><strong>Matriculado na Faculdade:</strong> {candidate.matriculadoFaculdade}</p>
+              <p><strong>Escolaridade:</strong> {candidate.escolaridade}</p>
+              <p><strong>Tempo de Experiência:</strong> {candidate.tempoDeExperiencia} meses</p>
+              <p><strong>Tempo no Último Emprego:</strong> {candidate.tempoNoUltimoEmprego} meses</p>
+              <p><strong>Horas de Treinamento:</strong> {candidate.horasDeTreinamento}</p>
+              <p><strong>Último Salário:</strong> R${candidate.ultimoSalario}</p>
+              <p><strong>Percentual de Compatibilidade:</strong> {candidate.percentualCompatibilidade}%</p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
       <button onClick={onRestart}>Tentar Novamente</button>
     </div>
   );
